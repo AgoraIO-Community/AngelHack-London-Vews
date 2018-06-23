d940ed517380947077d3284c9ea7b15894d34ff6
Luca Marchal <lucamarchal@gmail.com>
2018-06-23 13:31:06 +0100

    Stream

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
+import AgoraRTC from 'agora-rtc-sdk';
 import React, { Component } from 'react';
 import firebase from "./firebase";
 
 import "./App.scss";
 
 class App extends Component {
-  state = {
-    topic: "Current Topic"
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      topic: "Current Topic",
+      client: null
+    };
+  }
 
   componentDidMount() {
     this.topicRef = firebase.database().ref("/topic");
     this.topicCallback = this.topicRef.on('value', (snap) => {
       this.setState({ topic: snap.val() });
+    });
+
+    var client = AgoraRTC.createClient({mode:'interop'});
+
+    client.init("8afc4d7d7acf4d10a4014c306d7153c1", function(){
+      console.log("AgoraRTC client initialized");
+    });
+
+    client.join(null, "webtest", undefined, (uid) => {
+      console.log("User " + uid + " join channel successfully");
+      console.log("Timestamp: " + Date.now());
+
+      let localStream = AgoraRTC.createStream({
+        streamID: uid,
+        audio: true,
+        video: true,
+        screen: false
+      })
+
+      localStream.setVideoProfile("480p_4");
+
+      localStream.init(function(){
+        console.log("Local stream initialized");
+        client.publish(localStream, function(err){
+          console.log("Publish stream failed", err);
+        });
+      });
+
+      this.setState({
+        client
+      })
     });
   }
 
