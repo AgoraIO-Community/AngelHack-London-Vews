3538362a7493e99a2d97c4959b436f00928a184f
MrBBot <me@mrbbot.co.uk>
2018-06-23 12:45:34 +0100

    Firebase stuff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import firebase from "./firebase";
 
 import "./App.scss";
 
 class App extends Component {
+  state = {
+    topic: "Current Topic"
+  };
+
+  componentDidMount() {
+    this.topicRef = firebase.database().ref("/topic");
+    this.topicCallback = this.topicRef.on('value', (snap) => {
+      this.setState({ topic: snap.val() });
+    });
+  }
+
+  componentWillUnmount() {
+    this.topicRef.off("value", this.topicCallback);
+  }
+
   render() {
     return (
       <div className="App">
